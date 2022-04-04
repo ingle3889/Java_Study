9dbdf5df9bccc74cd7b441f02ee87d9c1d96feaf
Ramprasad Ingle <95127166+ingle3889@users.noreply.github.com>
2022-04-05 00:12:40 +0530

    Add files via upload

---
+++ Fileread.java
@@ -0,0 +1,9 @@
+import java.io.FileReader; 
+import java.util.List; 
+ 
+
+public class Fileread{
+	public static void main(String args[]){
+	Filereader File = new Filereader("sample4.csv");
+	}
+			}
---
+++ Removesecchar.java
@@ -0,0 +1,12 @@
+public class Removesecchar  
+	{  
+		public static void main(String args[])   
+		{  
+			String str= "123-345-6789";   
+			str = str.replaceAll("[-]", "");  
+			System.out.println(str);  
+		}  
+	} 
+table, th, td {
+  border: 1px solid black;
+}
